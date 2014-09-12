245fd5c55bd22461b874ccc3945f6fb09066d6c5
Yiğit Korkmaz <mkp.yigit@gmail.com>
2014-09-12 16:33:20 +0300

    Update Program.cpp

--- Program.cpp
+++ Program.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+using namespace std;
+
+main() {
+  
+  int yas;
+  cout << "Yasinizi giriniz : ";
+  cin >> yas;
+  
+  if(yas<18)
+    cout << "Yasiniz oy kullanmak icin yeterli degil";
+  if(yas>18)
+    cout << "Yasiniz oy kullanmak icin yeterli";
+  if(yas=18)
+  	 cout << "Yasiniz oy kullanmak icin yeterli";
+}
